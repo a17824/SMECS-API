cbe1e6c0e031545280529014d3e3d22541bb8a58
Ricardo Alves <RicardoNAlves@gmail.com>
2018-01-23 13:49:52 -0500

    test

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ module.exports = {
     'database': 'mongodb://localhost/SMECS_database'
     //'database': 'mongodb://76.118.200.189/SMECS_database'
     //'database': 'mongodb://ricky:database2@ds249415.mlab.com:49415/smecs_database'
+    //test
 };
