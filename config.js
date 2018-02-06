da92831c79a3185784314b95264d8a665f44138d
a17824 <pdcpadr@gmail.com>
2018-02-06 14:00:48 -0500

    mongo with remote access on different port with authentication

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-    //'database': 'mongodb://192.168.3.248/SMECS_database'
-    'database': 'mongodb://localhost/SMECS_database'
-    //'database': 'mongodb://76.118.200.189/SMECS_database'
+    //'database': 'mongodb://a17824:Abington2018@192.168.3.248:2999/SMECS_database'
+    'database': 'mongodb://a17824:Abington2018@localhost:2999/SMECS_database'
+    //'database': 'mongodb://a17824:Abington2018@76.118.200.189:2999/SMECS_database'
     //'database': 'mongodb://ricky:database2@ds249415.mlab.com:49415/smecs_database'
 
 };
